65d2ae9572d843080a02cd59c9c9a2691f6c69f9
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-07-28 17:16:45 +0530

    Update DataSupplierTest.java

--- src/test/java/com/afk/DataSupplierTest.java
+++ src/test/java/com/afk/DataSupplierTest.java
@@ -4,11 +4,20 @@ import com.afk.testdata.TestData;
 import com.afk.utils.DataProviderUtils;
 import org.testng.annotations.Test;
 
+/**
+ * DataSupplierTest class demonstrates the usage of data-driven testing in TestNG.
+ * This class utilizes the DataProvider feature to supply test data from an external source
+ * and executes the test method for each set of data.
+ */
 public class DataSupplierTest {
 
-
-
-    @Test(dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+    /**
+     * Test method that receives data from the DataProvider.
+     * The method prints the username, password, and expected title from the TestData object.
+     *
+     * @param testData The TestData object containing the data for the test case.
+     */
+    @Test(dataProvider = "getData", dataProviderClass = DataProviderUtils.class)
     public void dataSupplierTest(TestData testData) {
         System.out.println("Username: " + testData.getUsername());
         System.out.println("Password: " + testData.getPassword());
