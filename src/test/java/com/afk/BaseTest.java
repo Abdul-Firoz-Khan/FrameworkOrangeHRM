ea2ad7e51608102c75ba7e63c58de8b4e18c0f9b
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-07-28 17:16:24 +0530

    Update BaseTest.java

--- src/test/java/com/afk/BaseTest.java
+++ src/test/java/com/afk/BaseTest.java
@@ -8,30 +8,46 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import java.lang.reflect.Method;
 
+/**
+ * BaseTest serves as the base class for all test classes in the framework.
+ * It includes setup and teardown methods that are executed before and after each test method and test suite.
+ * The class handles the initialization and cleanup of the WebDriver, as well as the setup and finalization of test reports.
+ */
 public class BaseTest {
 
-
+    /**
+     * Initializes the ExtentReports at the beginning of the test suite.
+     */
     @BeforeSuite
     public void beforeSuite() {
         ExtentReport.initReport();
     }
 
+    /**
+     * Flushes the ExtentReports at the end of the test suite.
+     */
     @AfterSuite
     public void afterSuite() {
         ExtentReport.flushReports();
     }
 
+    /**
+     * Sets up the test environment before each test method, including creating a test report entry
+     * and initializing the WebDriver.
+     *
+     * @param method The test method that is being executed.
+     */
     @BeforeMethod
     public void setUp(Method method) {
         ExtentReport.createTest(method.getName());
         Driver.initDriver();
-
     }
 
+    /**
+     * Cleans up the test environment after each test method, including quitting the WebDriver.
+     */
     @AfterMethod
     public void tearDown() {
         Driver.quiteDriver();
-
     }
 }
-
