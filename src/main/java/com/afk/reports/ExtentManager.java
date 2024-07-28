ba21c92f4a9ed68831f665031b8164fef02959d8
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-07-28 17:14:06 +0530

    Update ExtentManager.java

--- src/main/java/com/afk/reports/ExtentManager.java
+++ src/main/java/com/afk/reports/ExtentManager.java
@@ -2,18 +2,34 @@ package com.afk.reports;
 
 import com.aventstack.extentreports.ExtentTest;
 
+/**
+ * ExtentManager is a utility class that manages the ExtentTest instances for reporting.
+ * It uses ThreadLocal to ensure thread safety, allowing each thread to have its own
+ * instance of ExtentTest.
+ */
 public final class ExtentManager {
     private ExtentManager() {
+        // Private constructor to prevent instantiation
     }
 
+    // ThreadLocal to hold ExtentTest instances for each thread
     private static final ThreadLocal<ExtentTest> threadLocal = new ThreadLocal<>();
 
+    /**
+     * Retrieves the ExtentTest instance for the current thread.
+     *
+     * @return The ExtentTest instance for the current thread.
+     */
     static ExtentTest getExtentTest() {
         return threadLocal.get();
     }
 
+    /**
+     * Sets the ExtentTest instance for the current thread.
+     *
+     * @param test The ExtentTest instance to set for the current thread.
+     */
     static void setExtentTest(ExtentTest test) {
         threadLocal.set(test);
     }
-
 }
