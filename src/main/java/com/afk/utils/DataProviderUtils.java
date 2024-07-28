a3dbed7babe74785e5f1dfbbb65989085cb0f045
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-07-28 17:15:17 +0530

    Update DataProviderUtils.java

--- src/main/java/com/afk/utils/DataProviderUtils.java
+++ src/main/java/com/afk/utils/DataProviderUtils.java
@@ -6,8 +6,19 @@ import io.github.sskorol.data.TestDataReader;
 import io.github.sskorol.data.XlsxReader;
 import one.util.streamex.StreamEx;
 
+/**
+ * DataProviderUtils is a utility class that provides test data for test cases.
+ * It uses the DataSupplier annotation from the SSKorol library to supply test data
+ * in parallel from an Excel file.
+ */
 public class DataProviderUtils {
 
+    /**
+     * Supplies test data in parallel from the "testdata.xlsx" file.
+     * The data is read into a stream of TestData objects.
+     *
+     * @return A StreamEx object containing the test data.
+     */
     @DataSupplier(runInParallel = true)
     public StreamEx<TestData> getData() {
         return TestDataReader.use(XlsxReader.class)
