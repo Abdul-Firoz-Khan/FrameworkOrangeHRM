65d1f0c6b06896ce7a2ef7164c754ee9ee52804d
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-07-28 17:12:49 +0530

    Update HomePageOrangeHRM.java

--- src/main/java/com/afk/pages/HomePageOrangeHRM.java
+++ src/main/java/com/afk/pages/HomePageOrangeHRM.java
@@ -2,8 +2,19 @@ package com.afk.pages;
 
 import com.afk.driver.DriverManager;
 
+/**
+ * HomePageOrangeHRM represents the home page of the OrangeHRM application.
+ * This class provides methods to interact with elements on the home page,
+ * such as retrieving the page title.
+ */
 public class HomePageOrangeHRM {
-    public String getHomePageTitle(){
-        return DriverManager.getDriver().getTitle();
 
-}}
+    /**
+     * Retrieves the title of the home page.
+     *
+     * @return The title of the home page as a String.
+     */
+    public String getHomePageTitle() {
+        return DriverManager.getDriver().getTitle();
+    }
+}
